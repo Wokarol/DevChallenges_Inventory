cf2310c2f76721da58d279c73dfd3ee3ac48f56a
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-05-24 20:08:57 +0200

    Add TMP, Add DOTween, Add other packages

---
+++ Assets/_Shared/Scripts/Runtime/UI/Fancy Cursor System/FancyCursor.cs
@@ -0,0 +1,192 @@
+using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wokarol.Common.UI
+{
+    public class FancyCursor : MonoBehaviour
+    {
+        [SerializeField] private Image cursorImage;
+        [SerializeField] private float scaleBy = 0.1f;
+        [SerializeField] private float scaleDownDuration = 0.1f;
+        [SerializeField] private float scaleUpDuration = 0.3f;
+        [Space]
+        [SerializeField] private Sprite defaultCursor;
+        [SerializeField] private Sprite handPointerCursor;
+        [SerializeField] private Sprite linearSlideCursor;
+        [SerializeField] private Sprite rotationCursor;
+        [SerializeField] private Sprite textCursor;
+
+        private List<CursorDriver> drivers = new();
+        private CursorType lastStateType;
+        private Tween imageSwapTween;
+
+        private void Start()
+        {
+            Cursor.visible = false;
+        }
+
+        void Update()
+        {
+            var screenPos = Input.mousePosition;
+            cursorImage.transform.position = screenPos;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                cursorImage.transform.DOBlendableScaleBy(Vector3.one * -scaleBy, scaleDownDuration);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                cursorImage.transform.DOBlendableScaleBy(Vector3.one * scaleBy, scaleUpDuration);
+            }
+
+
+            CursorState state = GetCurrentState(screenPos);
+            if (lastStateType != state.Type)
+            {
+                imageSwapTween.Kill(true);
+                imageSwapTween = DOTween.Sequence()
+                    .Append(cursorImage.transform.DOBlendableScaleBy(Vector3.one * 0.2f, 0.1f))
+                    .AppendCallback(() =>
+                    {
+                        cursorImage.sprite = GetSpriteFromType(state.Type);
+                    })
+                    .Append(cursorImage.transform.DOBlendableScaleBy(Vector3.one * -0.2f, 0.1f))
+                    .SetLink(gameObject).SetTarget(cursorImage)
+                    .SetUpdate(true);
+                
+            }
+            lastStateType = state.Type;
+
+            //cursorImage.transform.rotation = Quaternion.Euler(0, 0, state.Rotation);
+
+        }
+
+        private void OnApplicationFocus(bool focus)
+        {
+            if (focus) Cursor.visible = false;
+        }
+
+        public void AddDriver(GameObject caller, Func<CursorState> getter, Func<bool> isPriority = null) => AddDriver(caller, d => getter(), isPriority);
+        public void AddDriver(GameObject caller, Func<CursorGetterData, CursorState> getter, Func<bool> isPriority = null)
+        {
+            if (drivers.Any(d => d.Owner == caller))
+                return;
+
+            drivers.Add(new()
+            {
+                Owner = caller,
+                Getter = getter,
+                IsPriorityGetter = isPriority,
+            });
+
+            if (drivers.GroupBy(c => c.Owner.transform.root).Any(g => g.Count() >= 2))
+            {
+                drivers.Sort((x, y) =>
+                {
+                    if (x.Owner.transform.IsChildOf(y.Owner.transform))
+                    {
+                        return -1;
+                    }
+                    if (y.Owner.transform.IsChildOf(x.Owner.transform))
+                    {
+                        return 1;
+                    }
+                    return 0;
+                });
+            }
+        }
+
+        public void RemoveDriver(GameObject caller)
+        {
+            int index = drivers.FindIndex(d => d.Owner == caller);
+
+            if (index < 0)
+            {
+                throw new Exception($"Trying to remove a driver but none matches the caller ({caller.name})");
+            }
+
+            drivers.RemoveAt(index);
+        }
+
+        private Sprite GetSpriteFromType(CursorType type)
+        {
+            return type switch
+            {
+                CursorType.Default => defaultCursor,
+                CursorType.HandPoint => handPointerCursor,
+                CursorType.Linear => linearSlideCursor,
+                CursorType.Circular => rotationCursor,
+                CursorType.Text => textCursor,
+                _ => null,
+            };
+        }
+        private CursorState GetCurrentState(Vector2 screenPos)
+        {
+            if (drivers.Count == 0)
+            {
+                return new CursorState()
+                {
+                    Type = CursorType.Default,
+                    Rotation = 0,
+                };
+            }
+            else
+            {
+                var selectedDriver = drivers[0];
+
+                if (drivers.Count >= 2)
+                    foreach (var driver in drivers)
+                    {
+                        if (driver.IsPriorityGetter?.Invoke() ?? false)
+                        {
+                            selectedDriver = driver;
+                            break;
+                        }
+                    }
+
+                return selectedDriver.Getter(new()
+                {
+                    MousePosition = screenPos,
+                });
+            }
+        }
+
+        private struct CursorDriver
+        {
+            public GameObject Owner;
+            public Func<CursorGetterData, CursorState> Getter;
+            public Func<bool> IsPriorityGetter;
+        }
+
+        public struct CursorState
+        {
+            public CursorType Type;
+            public float Rotation;
+
+            public CursorState(CursorType type, float rotation = 0)
+            {
+                Type = type;
+                Rotation = rotation;
+            }
+        }
+
+        public struct CursorGetterData
+        {
+            public Vector2 MousePosition;
+        }
+
+        public enum CursorType
+        {
+            Default,
+            HandPoint,
+            Linear,
+            Circular,
+            Text,
+        }
+    }
+
+}
---
+++ Assets/_Shared/Scripts/Runtime/UI/GenericBindableView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Wokarol.Common.UI
+{
+    public abstract class GenericBindableView<TargetT> : MonoBehaviour where TargetT : Component
+    {
+        public TargetT BoundTarget { get; private set; }
+        public bool IsBound { get; private set; }
+
+        protected virtual void Start()
+        {
+            UnbindAndHide(false);
+        }
+
+        private void LateUpdate()
+        {
+            UnbindIfTargetIsDestroyed();
+
+            if (IsBound)
+            {
+                UpdateView();
+            }
+        }
+
+        private void UnbindIfTargetIsDestroyed()
+        {
+            if (IsBound && BoundTarget == null)
+            {
+                UnbindAndHide();
+            }
+        }
+
+        public void BindAndShow(TargetT target, bool animated = true)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            bool hadTarget = BoundTarget != null;
+
+            BoundTarget = target;
+            IsBound = true;
+
+            OnBindAndShow(hadTarget, animated);
+        }
+
+        public void UnbindAndHide(bool animated = true)
+        {
+            bool hadTarget = BoundTarget != null;
+
+            BoundTarget = null;
+            IsBound = false;
+
+            OnUnbindAndHide(hadTarget, animated);
+        }
+
+        protected virtual void UpdateView() { }
+        protected virtual void OnBindAndShow(bool hadTarget, bool animated) { }
+        protected virtual void OnUnbindAndHide(bool hadTarget, bool animated) { }
+    }
+}
